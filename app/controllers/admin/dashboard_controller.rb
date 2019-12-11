b75976a19644abd2573c634dd38b30c194402909
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-12-11 23:59:34 +0000

    implement the show file for the admin and tables for dashbord

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASSWORD']
   def show
+    @product_count = Product.count(:all)
+    @category_count = Category.count(:all)
   end
 end
